2478ca34ad8e26ea3b34abc4e79b2f26c9eb4038
bhavyaupta1 <bhavyagupta8299460915@gmail.com>
2020-10-29 17:10:56 +0530

    inhertance code

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
-
+import Shape from './shape';
 
 // export class using module.exports
+export default class Circle extends Shape{
+    constructor(){
+         super();
+    }
+    calculateArea(){
+        console.log("area of a circle"+this.color);
+        return 3.14*2*2;
+    }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+export default class Shape{
+    constructor(){
+        this.color="red";
+    }
+    drawShape(){
+          console.log("draw a shape");
+    }
+    calculateArea(){
+         console.log("Dont want to calculate now");
+    }
+}
+
